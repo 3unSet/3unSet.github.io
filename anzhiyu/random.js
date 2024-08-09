d6541e4a8eaecd0790d067f287d0b551ded900ea
3unset <2689600440@qq.com>
2024-08-09 16:23:29 +0800

    Site updated: 2024-08-09 16:23:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/04/hello-world/","2024/07/28/HJM/"];function toRandomPost(){
+var posts=["2024/07/28/HJM/","2024/07/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
